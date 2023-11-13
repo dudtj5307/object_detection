8ccee2700ff7b95113db5b69303126fb4c469717
Youngsuh Lee <yslee5307@gmail.com>
2023-11-13 22:02:24 +0900

    change_image_size_by_rate

--- face_recognition.py
+++ face_recognition.py
@@ -12,6 +12,9 @@ for img in glob.glob("image/*.jpg"):
         
         # img = np.float32(img)
         cv2.rectangle(img, (f.left(), f.top()), (f.right(), f.bottom()), (255, 0, 0), 2)
-
+    
+    print(img.shape)
+    change_size = 1000
+    img = cv2.resize(img, dsize=(int(img.shape[1]*change_size/img.shape[0]), change_size))
     cv2.imshow("1", img)
     cv2.waitKey(0) 
